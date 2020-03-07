49c4ce1546b5a14c9ab50eb7ecbf5e4877171843
Jacob Earle <jacob.earle@yale.edu>
2020-03-07 15:36:08 -0500

    Added wealth appreciation and printing functions

--- calc.cc
+++ calc.cc
@@ -1,7 +1,29 @@
-#include <vector>
 #include <cmath>
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+//function to find the greatest common divisor of integers a and b using Euclid's Algorithm
+//a is assumed to be greater than or equal to b
+int gcd(int a, int b){
+    return b==0 ? a : gcd(b, a % b);
+}
+
+//this function will find the lowest common multiple of a and b using the gcd function defined above
+int lcm(int a, int b){
+    //ordering the numbers
+    int max, min;
+    if(a > b){
+        max = a;
+        min = b;
+    }
+    else{
+        max = b;
+        min = a;
+    }
+    int greatest_factor = gcd(max, min);
+    return max / greatest_factor * min;
+}
 
 int main(int argc, char * argv[]){
     //Initializing variables
@@ -85,11 +107,57 @@ int main(int argc, char * argv[]){
         string str;
         cin.clear();
         getline( cin, str);
-        cout << str << " is not a number.\n";
+        cout << str << " is not an integer.\n";
     }
     if(deposits < 1){
         cout << "Number of contributions must be at least one. Exiting.\n";
         exit(0);
+    }
+
+    //now, we will begin the process of calculating the appreciation of the wealth
+    //we will first calculate the value the period interest rate and the amount of money invested in each deposit
+    double period_rate = interest_rate / compounding_periods;
+    double period_deposit = income / deposits;
+    //now, we will synchronize the depositing and compounding periods using their lowest common multiple
+    int sync_rate = lcm(deposits, compounding_periods);
+    int deposit_sync_factor = sync_rate / deposits; 
+    int compounding_sync_factor = sync_rate / compounding_periods;
+    //defining an array to contain the amount of money at the end of each year
+    double wealth[years + 1];
+    wealth[0] = principle;
+    //initializing a variable to contain our current value
+    double current_value = principle;
+    int deposit_counter, compound_period_counter;
+    //this loop will track the value of the wealth every year
+    for(int year = 1; year <= years; year ++){
+        //in this loop, we will replicate the effect of compounding periods and income deposits
+        deposit_counter  = deposit_sync_factor;
+        compound_period_counter = compounding_sync_factor;
+        while((deposit_counter <= sync_rate) || (compound_period_counter <= sync_rate) ){
+            if(compound_period_counter <= deposit_counter){
+                //compounding wealth for one period
+                current_value *= (1 + period_rate);
+                compound_period_counter += compounding_sync_factor;
+            }
+            else{
+                //depositing income for one period
+                current_value += period_deposit;
+                deposit_counter += deposit_sync_factor;
+            }
+        }
+        
+        //we have finished all of the compounding and deposit periods for the year, so we will updat e the array and then account for growing income 
+        wealth[year] = current_value;
+        period_deposit *= (1 + income_appreciation);
+    }
+
+    //now we will print the results
+    for(int i = 0; i <= years; i ++){
+        //setting precision settings
+        cout.setf(ios::fixed, ios::floatfield);
+        cout.precision(2);
+        //outputting wealth in that year
+        cout << "Value in year " << setw(2) << i << ": " << setw(15) << wealth[i] << endl;
     }
     return 0;
 }
